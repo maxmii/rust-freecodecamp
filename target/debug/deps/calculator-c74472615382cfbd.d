05883a2ec1c1209865d487d27ddbe5fa2ef7c1c6
maxmii <morrm046@gmail.com>
2022-08-29 23:41:06 +0100

    Remove debug

--- target/debug/deps/calculator-c74472615382cfbd.d
+++
@@ -1,5 +0,0 @@
-/Users/maxmorris/Documents/Projects/rust/Rust-In-Replit/target/debug/deps/calculator-c74472615382cfbd.rmeta: calculator/src/main.rs
-
-/Users/maxmorris/Documents/Projects/rust/Rust-In-Replit/target/debug/deps/calculator-c74472615382cfbd.d: calculator/src/main.rs
-
-calculator/src/main.rs:
--- target/debug/deps/fcc-3f3c8e4097bd2dcc.d
+++
@@ -1,5 +0,0 @@
-/Users/maxmorris/Documents/Projects/rust/Rust-In-Replit/target/debug/deps/fcc-3f3c8e4097bd2dcc.rmeta: src/main.rs
-
-/Users/maxmorris/Documents/Projects/rust/Rust-In-Replit/target/debug/deps/fcc-3f3c8e4097bd2dcc.d: src/main.rs
-
-src/main.rs:
